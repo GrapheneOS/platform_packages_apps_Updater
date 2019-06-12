9d29020f289bc284ee2a81649332e897664bc741
Daniel Micay <danielmicay@gmail.com>
2019-06-12 11:29:59 -0400

    avoid unnecessarily using system API

--- src/app/seamlessupdate/client/BootReceiver.java
+++ src/app/seamlessupdate/client/BootReceiver.java
@@ -9,7 +9,7 @@ import android.os.UserManager;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
-        if (UserManager.get(context).isSystemUser()) {
+        if (context.getSystemService(UserManager.class).isSystemUser()) {
             Settings.getPreferences(context).edit().putBoolean(Settings.KEY_WAITING_FOR_REBOOT, false).apply();
             PeriodicJob.schedule(context);
         } else {
